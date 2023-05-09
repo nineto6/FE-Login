ea5c7441ccfa33dff4c4b47388ff45feb6c83d01
ezurno <ezurno@gmail.com>
2023-05-09 16:08:45 +0900

    SET: get authorization & store localStorage
    1. BE 에서 준 header 의 authorization 을 받아옴
    
    2. 받아온 authorization 을 ACCESS_TOKEN 으로 초기화 한 후 localStorage 에 담아 저장
    
    3. localStorage 는 브라우저 내 token 을 저장해 다시 켜도 해당 값을 사용하기 위함 (login, logout)
    
    4. 응답의 header (response.header) 는 JSON 형식이므로 그냥 접근하면 null or undefined 오류가 나므로 JSON.stringify 로 수정해 접근해야 함

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,45 @@
 import axios from "axios";
 import { useMutation } from "react-query";
 import { IFormData } from "./App";
+import { json } from "stream/consumers";
 
-const API_URL = `http://nineto6.kro.kr:8080/api/user/login`;
+const API_URL = `https://nineto6.kro.kr:8080/api/user/login`;
 // 변경된 URL 주소
 
 export async function getData() {
-  return await fetch(API_URL).then((response) => response.json());
+  return await fetch(API_URL).then((response) => {
+    let ACCESS_TOKEN = response.headers.get("Authorization");
+    if (ACCESS_TOKEN) {
+      localStorage.setItem("loginToken", ACCESS_TOKEN);
+      // authorization 이 존재할 경우 localStorage 에 leginToken 으로 ACCESS_TOKEN 임시저장
+    }
+    response.json();
+  });
 }
 
 export const OnPostData = () => {
   return useMutation(postData);
 };
 
-export const postData = (data: IFormData) => {
-  return axios.post(API_URL, data);
+export const postData = async (data: IFormData) => {
+  return await axios.post(API_URL, data).then((response) => {
+    // const { autorization } = response.headers;
+    if (response.status === 200) {
+      axios
+        .post(API_URL, data)
+        .then((response) => {
+          console.log(response);
+          let ACCESS_TOKEN = JSON.stringify(response.headers["authorization"]);
+          // JSON 형식이므로 JSON.stringify 를 사용해 주어야 한다. (*대소문자 주의*)
+          // ACCESS_TOKEN 으로 초기화
+          console.log(ACCESS_TOKEN);
+          localStorage.setItem("loginToken", ACCESS_TOKEN);
+
+          console.log(localStorage.getItem("loginToken"));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
 };
